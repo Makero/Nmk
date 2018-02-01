5b8371d0fd23bc8deb69fd1a0687f2edc20e8023
Maker <lcwjyouyi@163.com>
2018-02-01 19:29:46 +0800

    邀请函内容补充

--- public/js/invitation.js
+++ public/js/invitation.js
@@ -1,31 +1,37 @@
 $(function() {
-    var animateName,$Div;
-    var slider = $.fn.fsvs({
-        speed: 300,
-        bodyID: 'fsvs-body',
-        selector: '> .slide',
-        mouseSwipeDisance: 40,
-        afterSlide: function (n) {
-            $Div.show();
-            $Div.addClass(animateName).show();
-        },
-        beforeSlide: function (n) {
-            $Div = $($(".slide")[n]).children(".animated");
-            animateName = $Div.attr("data-animate");
-            $Div.removeClass(animateName).hide();
-        },
-        endSlide: function () {
-        },
-        mouseWheelEvents: true,
-        mouseWheelDelay: false,
-        scrollabelArea: 'scrollable',
-        mouseDragEvents: true,
-        touchEvents: true,
-        arrowKeyEvents: true,
-        pagination: true,
-        nthClasses: false,
-        detectHash: true
-    });
+    setTimeout(function(){
+        $(".imgInv").show();
+        var animateName,$Div;
+        var slider = $.fn.fsvs({
+            speed: 300,
+            bodyID: 'fsvs-body',
+            selector: '> .slide',
+            mouseSwipeDisance: 40,
+            afterSlide: function (n) {
+                $Div.show();
+                $Div.addClass(animateName).show();
+            },
+            beforeSlide: function (n) {
+                $Div = $($(".slide")[n]).children(".animated");
+                animateName = $Div.attr("data-animate");
+                $Div.removeClass(animateName).hide();
+            },
+            endSlide: function () {
+            },
+            mouseWheelEvents: true,
+            mouseWheelDelay: false,
+            scrollabelArea: 'scrollable',
+            mouseDragEvents: true,
+            touchEvents: true,
+            arrowKeyEvents: true,
+            pagination: true,
+            nthClasses: false,
+            detectHash: true
+        });
+        setTimeout(function(){
+            $(".describe").show();
+            $("#write").MKtypewriter(30);
+        },1000);
+    },1000);
 
-    $("#write").MKtypewriter(60);
 });
