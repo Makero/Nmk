a00fb0a02f4f55d1a680cdf38636ea29933962e7
cmMaker <lccm722@yahoo.com>
2018-06-04 23:37:40 +0800

    读取远程音乐文件写入本地

--- routes/music_route.js
+++ routes/music_route.js
@@ -1,4 +1,6 @@
 const router = require('koa-router')();
+const fs = require('fs');
+const request = require('request');
 const songController = require('../controllers/music_controller');
 
 router.get('/', async (ctx, next) => {
@@ -8,10 +10,32 @@ router.get('/', async (ctx, next) => {
         params: ctx.query
     });
     console.log(ctx.api);
+
     await ctx.render('music', {
         title: '音乐',
         data: ctx.api.data
     })
+});
+
+router.get('/play/:id', async (ctx, netx) => {
+
+    wstream  = fs.createWriteStream(process.cwd()+"/temp/"+ ctx.params.id +".mp3");
+    console.log(ctx.params.id);
+    request(ctx.query.url).pipe(wstream).on("close", function(err){
+        console.log("ok");
+        stream  = fs.createReadStream(process.cwd()+"/temp/"+ ctx.params.id +".mp3");
+        let responseData = [];//存储文件流
+        if (stream) {//判断状态
+            stream.on( 'data', function( chunk ) {
+                responseData.push( chunk );
+            });
+            stream.on( 'end', function() {
+                let finalData = Buffer.concat( responseData );
+                ctx.response.status = 200;
+                ctx.response.body =  finalData;
+            });
+        }
+    });
 });
 
 module.exports = router;
