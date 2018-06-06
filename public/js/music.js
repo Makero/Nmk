24242915ca432986ac49dd3037429320e4d74b2d
cmMaker <lccm722@yahoo.com>
2018-06-06 08:38:57 +0800

    音乐播放页面

--- public/js/music.js
+++ public/js/music.js
@@ -1,9 +1,15 @@
 (() => {
+
     $(".start").click(function(){
         if($(this).hasClass("play")){
             $(this).removeClass("play").addClass("pause");
+            $(".pic_radio").removeClass("paused");
+            $("#audio")[0].play();
         }else{
             $(this).removeClass("pause").addClass("play");
+            $(".pic_radio").addClass("paused");
+            $("#audio")[0].pause();
         }
     });
+
 })();
