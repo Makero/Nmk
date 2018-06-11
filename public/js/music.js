13f7e2c46d8422543f7914eb8b8c2813f2bdaf11
maker <lcwjyouyi@163.com>
2018-06-11 08:43:12 +0800

    音乐歌词

--- public/js/music.js
+++ public/js/music.js
@@ -1,32 +1,40 @@
 
 const Music = {
     audio : $("#audio")[0],
-    timer : 0,
-    time : 0,
-    play : function(tag){
+    timer : 0, time : 0, roll : 0,
+    play : function(tag){//播放、暂停音乐
         if(this.audio.paused){
             $(tag).removeClass("play").addClass("pause");
             $(".pic_radio").removeClass("paused");
             this.audio.play();
+            this.progress(tag);
         }else{
             $(tag).removeClass("pause").addClass("play");
             $(".pic_radio").addClass("paused");
             this.audio.pause();
+            clearInterval(this.timer);
         }
     },
-    format : function(t){
+    format : function(t,d){//时间格式化
         let minute = parseInt(t/60);
-        let sec = parseInt(t-(minute*60));
+        let sec = t-(minute*60);
+        if(!d){
+            sec = parseInt(sec);
+        }else{
+            sec = sec.toFixed(d);
+        }
+
         if(minute<10) minute = '0'+minute;
         if(sec<10) sec = '0'+sec;
         return minute+":"+sec
     },
-    progress :function(tag){
+    progress : function(tag){//时间进度
         const self = this;
         clearInterval(self.timer);
         this.timer = setInterval(function(){
             let ctime = self.audio.currentTime;
             let percent = ctime/self.time * 100;
+            self.lrc_roll(self.format(ctime,2));
             $("#current").text(self.format(ctime));
             if(!self.audio.ended){
                 $("#progress").width(percent+"%");
@@ -36,9 +44,9 @@ const Music = {
                 $(".pic_radio").addClass("paused");
             }
 
-        },100);
+        },8);
     },
-    lrc : function(){
+    lrc : function(){//获取歌词
         $.ajax({
             url : "/music/ajax/lrc",
             type : 'get',
@@ -47,10 +55,22 @@ const Music = {
                 $("#lrc").html(result);
             }
         })
+    },
+    lrc_roll : function(t){//歌词同步滚动
+        $(".roll:not(:last)").removeClass("red");
+        $("p[data-time='"+t+"']").addClass("roll red");
+        if($("p[data-time='"+t+"']").hasClass("roll")){
+            let n = $(".lrc-box .roll").length;
+            this.roll = 30 * n;
+            $("#lrc").scrollTop(this.roll);
+
+        }
+
     }
 };
 
 (() => {
+    /** 动态的加载中提示 **/
     const loading = $("#loading").attr("data-load");
     let n = 3;
     const timer = setInterval(function(){
@@ -58,16 +78,17 @@ const Music = {
         $("#loading").text(loading.substring(0,loading.length-n));
         n--;
     },500);
-    Music.lrc();
+
+    /** 音乐加载完成后 **/
     Music.audio.addEventListener("canplaythrough", () => {
         clearInterval(timer);
         $("#loading").addClass("hide");
         $(".time").removeClass("hide");
         Music.time = Music.audio.duration;
         $("#duration").text(Music.format(Music.time));
-        $(".start").click(function(){
+        Music.lrc();
+        $(".start").unbind("click").click(function(){
             Music.play(this);
-            Music.progress(this);
         });
     }, false);
 })();
--- routes/music_route.js
+++ routes/music_route.js
@@ -2,10 +2,10 @@ const router = require('koa-router')();
 const songController = require('../controllers/music_controller');
 
 router.get('/', async (ctx, next) => {
-
+    console.log(ctx.query);
     await songController.getSong({
         ctx,
-        params: ctx.query
+        params: {songid:ctx.query.songid.split(',')[0]}
     });
     console.log(ctx.api);
     await ctx.render('music/music', {
@@ -19,7 +19,24 @@ router.get('/ajax/lrc', async(ctx, next) => {
         ctx,
         params: ctx.query
     });
-    console.log(ctx.api.data.lrcContent);
-    await ctx.render('music/part/lrc', {data: ctx.api.data});
+    let arr = '';
+    if(!ctx.api.data.error_code){
+        /** 将歌词文本转化成数组类型 **/
+        arr = ctx.api.data.lrcContent.replace(/\r/g,'').split('\n');
+        for(let n in arr){
+            arr[n].replace(/(\[.*\])/, function(rs, $1, offset, source){
+                let lrc = source.replace($1,'');
+                let time = $1.replace('[','').replace(']','');
+                let temp = time.split(".");
+                if(temp[1].length === 1) temp[1] = "0"+temp[1];
+                time = temp[0] +"."+ temp[1];
+                arr[n] = [time, lrc];
+            });
+            if(typeof(arr[n]) === "string"){
+                arr[n] = [0, arr[n]];
+            }
+        }
+    }
+    await ctx.render('music/part/lrc', {list: arr});
 });
 module.exports = router;
