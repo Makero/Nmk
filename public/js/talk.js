4bfac74dbf874960c8135fd146f015ac7812d292
maker <lcwjyouyi@163.com>
2018-06-20 13:05:14 +0800

    样式修改

--- public/js/talk.js
+++ public/js/talk.js
@@ -5,12 +5,12 @@
         success: function(result){
             console.log(result);
             wx.config({
-                debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
                 appId: result.appid, // 必填，公众号的唯一标识
                 timestamp: result.timestamp, // 必填，生成签名的时间戳
                 nonceStr: result.noncestr, // 必填，生成签名的随机串
                 signature: result.signature,// 必填，签名
-                jsApiList: ['startRecord', 'stopRecord'] // 必填，需要使用的JS接口列表
+                jsApiList: ['startRecord', 'stopRecord', 'translateVoice'] // 必填，需要使用的JS接口列表
             });
             wx.ready(function(){
 
@@ -32,15 +32,51 @@
         }
     });
 
-    $("#sub").click(function(){
-        $.ajax({
-            url: "/talk/ajax/qyk",
-            type: 'post',
-            data: {talk:$("#talk").val()},
-            success: function(result){
-                $("#audio").attr("src","/play/speech?talk="+result);
-            }
-        });
-    });
+    document.body.addEventListener('touchstart', function () { });
+    window.ontouchstart = function(e) {
+        e.preventDefault();
+    };
+
+    var tag = $("#start")[0];
+
+    tag.addEventListener('touchstart',touch, false);
+    tag.addEventListener('touchend',touch, false);
+
+    function touch (event){
+        var event = event || window.event;
+
+
+        switch(event.type){
+            case "touchstart":
+                console.log("1");
+                wx.startRecord();
+                break;
+            case "touchend":
+                console.log("2");
+                wx.stopRecord({
+                    success: function (res) {
+                        localId = res.localId;
+                        wx.translateVoice({
+                            localId: localId, // 需要识别的音频的本地Id，由录音相关接口获得
+                            isShowProgressTips: 1, // 默认为1，显示进度提示
+                            success: function (res) {
+                                $.ajax({
+                                    url: "/talk/ajax/qyk",
+                                    type: 'post',
+                                    data: {talk:res.translateResult},   // 语音识别的结果
+                                    success: function(result){
+                                        $("#audio").attr("src","/play/speech?talk="+result);
+                                    }
+                                });
+                            }
+                        });
+                    }
+                });
+                break;
+        }
+
+    }
+
+
 
 })();
