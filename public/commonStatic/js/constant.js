ef7c9dc4b3c3006f09c6393893c899db32fa24cc
cmMaker <lccm722@yahoo.com>
2018-07-15 14:35:07 +0800

    代码规范

---
+++ public/commonStatic/js/constant.js
@@ -0,0 +1,6 @@
+const COOKIE_VALIDITY_PERIOD = 365; //cookie有效期（单位：天）
+const AUTH_TOKEN_VALIDITY_PERIOD = 5; //authToken有效期（单位：天）
+const CLOSE_MSG_BOX_TIME = 3000; //关闭消息框时间（单位：毫秒）
+
+const WX_QR_MSG = "请使用公众号提供的扫码进行扫码登录"; //二维码提示消息
+const WX_NUMBER = "微信公众号：namehanr722"; //公众号信息
--- public/wechatStatic/js/constant.js
+++
@@ -1,2 +0,0 @@
-const COOKIE_VALIDITY_PERIOD = 365; //cookie有效期
-const CLOSE_MSG_BOX_TIME = 3000; //关闭消息框时间
