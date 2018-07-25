5b935b5181eb69d90b824340e7e9546ab102359e
Maker <lcwjyouyi@163.com>
2018-07-25 18:04:16 +0800

    模块

--- routes/blog/index_blog_route.js
+++ routes/blog/index_blog_route.js
@@ -1,11 +1,15 @@
 const router = require('koa-router')();
 const isLogin = require('../../middleware/is_login');
-
+const indexController = require('../../controllers/blog/index_controller');
 router.get('/', isLogin, async (ctx, next) => {
-    let data = {model:2};
-    data.theme = "主页";
+    let data = {model:2,title:'Maker空间'};
     data.motto = "面对过去，不要迷离；面对未来，不必彷徨；活在今天，你只要把自己完全“展示”给别人看。";
-    await ctx.render('blog/indexBlog',{data,artic:{},newart:{},sart:{}})
+    await indexController.getArticleList({
+        token: ctx.session.token,
+        params: {}
+    });
+    console.log(ctx.api);
+    await ctx.render('blog/indexBlog',{data, userData: ctx.session, artic:{},newart:{},sart:{}})
 });
 
 module.exports = router;
