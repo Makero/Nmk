aad2a5c69bbb58ccb6f621e96f4836d434812e1d
Maker <lcwjyouyi@163.com>
2018-09-19 18:00:59 +0800

    博客首页、login页

---
+++ public/blogStatic/js/login/choose.js
@@ -0,0 +1,45 @@
+function Work(){
+	var chushi=(function(){
+		move(".faqbox").set("opacity","1").set("left","30px").duration('.8s').end();
+		move(".video").set("opacity","1").set("right","30px").duration('.8s').end();
+		move(".titl").set("opacity","1").duration('1.2s').end(function(){
+			move(".cp").set("top","218px").duration('.6s').end();
+			move(".cpdap").set("top","228px").duration('.6s').end(function(){
+				move(".ht").set("top","410px").duration('1s').end();
+				move(".htdap").set("top","410px").duration('1s').end();
+				move(".cs").set("top","450px").duration('1.8s').end();
+				move(".csdap").set("top","450px").duration('1.8s').end(function(){
+					move(".yu").rotate(180).end(function(){
+						move(".yu").set("opacity","1").duration('.5s').end()
+						move(".yu").set("left","1100px").duration('1s').end(function(){
+								move(".yu").rotate(90).set("top","450px").duration('1s').end(function(){
+									move(".yu").rotate(0).duration('.8s').end();
+									$(".yu").animate({top:'355px',left:'290px'},"slow");
+								})
+							});
+						
+					});
+					
+				});
+			});
+			move(".ui").set("top","218px").duration('1.4s').end();
+			move(".uidap").set("top","228px").duration('1.4s').end();
+			move(".qd").set("top","218px").duration('2.2s').end();
+			move(".qddap").set("top","228px").duration('2.2s').end();
+		});
+			
+		
+	})();
+	$(".cus").on("mouseenter",function(){
+		move(this).scale(1.05).duration('.2s').end();
+	})
+	$(".cus").on("mouseleave",function(){
+		move(this).scale(1).duration('.2s').end();
+	})
+	$(".yu").on("mouseenter",function(){
+		move(this).rotate(180).end();
+	})
+	$(".yu").on("mouseleave",function(){
+		move(this).rotate(0).end();
+	})
+}
--- routes/blog/index_blog_route.js
+++ routes/blog/index_blog_route.js
@@ -2,7 +2,7 @@ const router = require('koa-router')();
 const isLogin = require('../../middleware/is_login');
 const indexController = require('../../controllers/blog/index_controller');
 router.get('/', async (ctx, next) => {
-    let data = {model:2,title:'Maker空间'};
+    let data = {model:2,title:'Makerの空间'};
     data.motto = "面对过去，不要迷离；面对未来，不必彷徨；活在今天，你只要把自己完全“展示”给别人看。";
     await indexController.getArticleList({
         ctx,
