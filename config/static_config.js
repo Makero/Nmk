dbfd841ea083229d608a66487facb1dabfc8d753
maker <lcwjyouyi@163.com>
2018-09-26 17:57:57 +0800

    static_config文件添加注释 index.html js文件路径bug修改

--- config/static_config.js
+++ config/static_config.js
@@ -1,8 +1,17 @@
-const url = "";
+/**
+ *
+ * 配置静态文件请求地址
+ * 参数URL为静态文件的地址，本地开发不设置值，调用项目内部的静态文件
+ * 项目部署，将项目中的静态文件全部拷贝到nginx代理的静态文件地址根目录下
+ * 并将其设置成nginx代理的静态文件的地址
+ *
+ **/
+//const URL = "http://localhost/mkfile";
+const URL = "";
 module.exports = {
-    zone_static     : url + "/static/zone",
-    wechat_static   : url + "/static/wechat",
-    common_static   : url + "/static/common",
-    media_path      : url + "/media",
-    root_url        : url
+    zone_static     : URL + "/static/zone",
+    wechat_static   : URL + "/static/wechat",
+    common_static   : URL + "/static/common",
+    media_path      : URL + "/media",
+    root_url        : URL
 };
